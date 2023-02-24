24be65b6c002f79b773e2421f129998e1a97146a
João Victor <joao.silveira@estudante.ufla.br>
2023-02-24 20:21:00 -0300

    feat(fomento) create fomento interface

---
+++ backend/Entities/Fomento.ts
@@ -0,0 +1,33 @@
+export interface IFomento {
+    countries: string | string[]
+    region: string | string[]
+    institution: string
+    program: string
+    call: string
+    supportType: string
+    organizations: string
+    sector: string
+    requiresPartner: boolean
+    partnerType: string
+    TRLmin: string
+    TRLmax: string
+    term: Date | string
+    objective: string
+    elegibility: string
+    expenses: string
+    minValueReembolsaveis: string
+    maxValueReembolsaveis: string
+    minValueSubvencao: string
+    maxValueSubvencao: string
+    supportTax: string
+    duration: string
+    tax: string
+    shortage: string
+    amortization: string
+    isESG: boolean
+    link: string
+    abgiInterest: boolean
+    status: string
+    areasOrtechnologies: string | string[]
+    lastRelease: Date
+}
