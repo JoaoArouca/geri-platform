204801e36188daf0aa453d3281842f9e52a16299
João Victor <joao.silveira@estudante.ufla.br>
2023-02-24 21:26:05 -0300

    feat(fomento) create fomento entitie

--- backend/Entities/Fomento.ts
+++ backend/Entities/Fomento.ts
@@ -31,3 +31,136 @@ export interface IFomento {
     areasOrtechnologies: string | string[]
     lastRelease: Date
 }
+
+export class Fomento {
+    props: IFomento;
+
+    constructor(props: IFomento) {
+        this.props = props;
+        // needs validations
+    }
+
+    get countries(): string | string[] {
+        return this.props.countries;
+    }
+
+    get region(): string | string[] {
+        return this.props.region;
+    }
+
+    get institution(): string {
+        return this.props.institution;
+    }
+
+    get program(): string {
+        return this.props.program;
+    }
+
+    get call(): string {
+        return this.props.call;
+    }
+
+    get supportType(): string {
+        return this.props.supportType;
+    }
+
+    get organizations(): string {
+        return this.props.organizations;
+    }
+
+    get sector(): string {
+        return this.props.sector;
+    }
+
+    get requiresPartner(): boolean {
+        return this.props.requiresPartner;
+    }
+
+    get partnerType(): string {
+        return this.props.partnerType;
+    }
+
+    get TRLmin(): string {
+        return this.props.TRLmin;
+    }
+
+    get TRLmax(): string {
+        return this.props.TRLmax;
+    }
+
+    get term(): string | Date {
+        return this.props.term;
+    }
+
+    get objective(): string {
+        return this.props.objective;
+    }
+
+    get elegibility(): string {
+        return this.props.elegibility;
+    }
+
+    get expenses(): string {
+        return this.props.expenses;
+    }
+
+    get minValueReembolsaveis(): string {
+        return this.props.minValueReembolsaveis;
+    }
+
+    get maxValueReembolsaveis(): string {
+        return this.props.maxValueReembolsaveis;
+    }
+
+    get minValueSubvencao(): string {
+        return this.props.minValueSubvencao;
+    }
+
+    get maxValueSubvencao(): string {
+        return this.props.maxValueSubvencao;
+    }
+
+    get supportTax(): string {
+        return this.props.supportTax;
+    }
+
+    get duration(): string {
+        return this.props.duration;
+    }
+
+    get tax(): string {
+        return this.props.tax;
+    }
+
+    get shortage(): string {
+        return this.props.shortage;
+    }
+
+    get amortization(): string {
+        return this.props.amortization;
+    }
+
+    get isESG(): boolean {
+        return this.props.isESG;
+    }
+
+    get link(): string {
+        return this.props.link;
+    }
+
+    get abgiInterest(): boolean {
+        return this.props.abgiInterest;
+    }
+
+    get status(): string {
+        return this.props.status;
+    }
+
+    get areasOrtechnologies(): string | string[] {
+        return this.props.areasOrtechnologies;
+    }
+
+    get stalastReleasetus(): Date {
+        return this.props.lastRelease;
+    }
+}
