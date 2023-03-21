71fd429e114125251955b1919a77105137c721fa
JoaoArouca <joao.silveira@estudante.ufla.br>
2023-03-21 17:52:35 -0300

    Remove comments

--- backend/src/repositories/inMemory/inMemoryRepository-createFomento.ts
+++ backend/src/repositories/inMemory/inMemoryRepository-createFomento.ts
@@ -1,34 +1,17 @@
-import { PrismaClient } from "@prisma/client";
 import { Fomento } from "../../entities/Fomento";
 import { IFomentoRepository } from "../FomentoRepository";
 
 export class InMemoryRepositoryCreateFomento implements IFomentoRepository {
     private globalDB: Fomento[] = [];
 
-    // async findByTitle(title: string): Promise<Fomento> {
-    //     const fomento = this.globalDB.find(fomento => fomento.title === title);
-
-    //     return fomento as Fomento;
-    // }
-
-    // async save(fomento: Fomento): Promise<void> {
-    //     this.globalDB.push(fomento);        
-    // }
-    private prisma = new PrismaClient();
-
     async findByTitle(title: string): Promise<Fomento> {
         const fomento = this.globalDB.find(fomento => fomento.title === title);
-        // const fomento = this.prisma.fomento.findFirst({
-        //     where: {title}
-        // })
 
         return fomento as Fomento;
     }
 
     async save(fomento: Fomento): Promise<void> {
-        console.log("cheguei");        
-        await this.prisma.fomento.create({data: fomento});
-        console.log("supabase");
-        
+        this.globalDB.push(fomento);        
     }
+
 }
